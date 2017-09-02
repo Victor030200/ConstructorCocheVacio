16bc8944b566742e743ae81e0d9d1f7322882bf1
VictoRezbeck <VictoRezbeck@FEOSHM>
2017-09-02 18:03:45 -0500

    Ejemplo Del Constructor Vacio

---
+++ src/ejemplodelcoche/ConstructorCoche.java
@@ -0,0 +1,24 @@
+/**
+ * Esta Clase Nos Ejemplifica El Uso De Un Constructor Vacio.
+ *
+ * @author: Victor Rodríguez Rodríguez 5IM6
+ * @version: 02/09/2017
+ */
+package ejemplodelcoche;
+
+/**
+ * Clase Ejemplo Coche, Con Constructor Vacio Declarando En String
+ *
+ */
+public class ConstructorCoche {
+
+    /**
+     * Declaración De Las Variables Privadas String
+     *
+     */
+    private String Gasolina;
+    private String Llaves;
+    private String Conductor;
+    private String Bateria;
+}
+//Cierre De La Clase
---
+++ src/ejemplodelcoche/EjemploDelCoche.java
@@ -0,0 +1,29 @@
+/**
+ * Esta Clase Nos Ejemplifica El Uso De Un Constructor Vacio.
+ *
+ * @author: Victor Rodríguez Rodríguez 5IM6
+ * @version: 02/09/2017
+ */
+package ejemplodelcoche;
+
+/**
+ * Clase Ejemplo Coche, Con Constructor Vacio
+*
+ */
+public class EjemploDelCoche {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+         * En Ésta Parte Se Declara El Constructor En La Clase ConstructorCoche,
+         * Se Crea El Objeto "miConstructor" Y Se Indica Que Es Vacía.
+         */
+        ConstructorCoche miConstructor = new ConstructorCoche();
+
+    }
+    // Cierre Del Metodo
+
+}
+//Cierre De La Clase
